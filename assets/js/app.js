f21d1fd239f7603ebdd5ec263a9c0261cfeb903b
s-codedev <s.codedevelopment@gmail.com>
2021-04-28 17:12:13 +0700

    change content

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+// window.addEventListener('loadstart', () => {
+//     window.addEventListener('scroll', disableScroll());
+// });
+// window.addEventListener('ended', () => {
+//     const preloader = document.querySelector('.preloader');
+//     preloader.classList.add('preloader-finish');
+//     enableScroll();
+// });
+
+// function disableScroll() {
+//     document.body.style.overflow = 'hidden';
+//     document.querySelector('html').scrollTop = window.scrollY;
+//     document.querySelector('nav').style.display = none;
+// }
+
+// function enableScroll() {
+//     document.body.style.overflow = null;
+//     document.querySelector('nav').style.display = '';
+// }
